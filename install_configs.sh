3daa200c82b0b5122c4fd86577807368f76e7436
zetaemme <mattia.zorzan@gmail.com>
2020-04-02 16:40:50 +0200

    Upadeted configs

--- install_configs.sh
+++ install_configs.sh
@@ -2,6 +2,9 @@
 mkdir ../../.config/i3 2> /dev/null
 mkdir ../../.config/kitty 2> /dev/null
 mkdir ../../.config/polybar 2> /dev/null
+mkdir ../../.config/rofi 2> /dev/null
+mkdir ../../.config/picom 2> /dev/null
+
 cp -rf ../dotfiles/i3/* ../../.config/i3/
 cp -rf ../dotfiles/kitty/* ../../.config/kitty/
 cp -rf ../dotfiles/polybar/* ../../.config/polybar/
@@ -10,5 +13,7 @@ cp -rf ../dotfiles/vim/.vimrc ../../
 cp -rf ../dotfiles/alias/.aliasrc ../../
 cp -rf ../dotfiles/mirrorlist/.get_updated_mirrirlist.sh ../../
 cp -rf ../dotfiles/intel_driver/20-intel.conf ../../../../etc/X11/xorg.conf.d/
+cp -rf ../dotfiles/rofi/* ../../.config/rofi/
+cp -rf ../dotfiles/picom/* ../../.config/picom/
 
-chmod 700 ../../.get_updated_mirrirlist.sh
+chmod +x ../../.get_updated_mirrirlist.sh
